69d13bd346ef03439713ef5c8e088ef0b89b0dd8
alexa <alexa@DESKTOP-J22173Q>
2021-04-13 00:19:22 +0200

    first commit

---
+++ src/test/java/com/bibliothequeBatch/bibliothequeBatch/BibliothequeBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliothequeBatch.bibliothequeBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

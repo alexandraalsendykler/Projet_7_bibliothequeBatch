8e0b705e3bfc06abc16f494fc637e075084058ad
alexa <alexa@DESKTOP-J22173Q>
2021-04-19 21:30:24 +0200

    updates

--- src/main/java/com/bibliothequeBatch/service/RelancePretTask.java
+++ src/main/java/com/bibliothequeBatch/service/RelancePretTask.java
@@ -1,22 +1,28 @@
 package com.bibliothequeBatch.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.bibliothequeBatch.model.Pret;
 import com.bibliothequeBatch.repository.PretProxy;
 
 // CETTE CLASSE SERT A ECRIRE LE CODE
 
 @Component
 public class RelancePretTask {
-	
+
 	@Autowired
 	private PretProxy pretProxy;
-	
-	@Scheduled(cron = "0 12 * * * *")
+
+	@Scheduled(cron = "0 12 * * * *", zone = "Europe/Paris")
 	public void execute() {
-		System.out.println("text");
+		Iterable<Pret> pret = pretProxy.getPretDateDeFin();
+
 	}
 
 }
+
+//System.out.println("text");
